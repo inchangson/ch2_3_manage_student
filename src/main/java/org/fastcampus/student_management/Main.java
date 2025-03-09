fdb8966790760ecdc07f97e732888d3687464b43
son-inchang <3721cha21.11@gmail.com>
2025-03-09 20:16:04 +0900

    test : add default test dataset

--- src/main/java/org/fastcampus/student_management/Main.java
+++ src/main/java/org/fastcampus/student_management/Main.java
@@ -1,7 +1,9 @@
 package org.fastcampus.student_management;
 
 import org.fastcampus.student_management.application.course.CourseService;
+import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
 import org.fastcampus.student_management.application.student.StudentService;
+import org.fastcampus.student_management.application.student.dto.StudentInfoDto;
 import org.fastcampus.student_management.repo.CourseRepository;
 import org.fastcampus.student_management.repo.StudentRepository;
 import org.fastcampus.student_management.ui.UserInputType;
@@ -25,6 +27,8 @@ public class Main {
     CourseController courseController = new CourseController(coursePresenter, courseService,
         studentPresenter);
     StudentController studentController = new StudentController(studentPresenter, studentService);
+
+    addTestData(studentService, courseService);
 
     studentPresenter.showMenu();
     UserInputType userInputType = studentController.getUserInput();
@@ -55,5 +59,26 @@ public class Main {
       studentPresenter.showMenu();
       userInputType = studentController.getUserInput();
     }
+  }
+
+  private static void addTestData(StudentService studentService, CourseService courseService) {
+    // add default test dataset
+    StudentInfoDto student1 = new StudentInfoDto("KIM", 28, "Seoul");
+    StudentInfoDto student2 = new StudentInfoDto("LEE", 33, "Suwon");
+
+    studentService.saveStudent(student1);
+    studentService.saveStudent(student2);
+
+    CourseInfoDto student1Course1 = new CourseInfoDto("math1", 300000, "MONDAY", "KIM", 10L);
+    CourseInfoDto student1Course2 = new CourseInfoDto("math2", 300000, "FRIDAY", "KIM", 10L);
+
+    courseService.registerCourse(student1Course1);
+    courseService.registerCourse(student1Course2);
+
+    CourseInfoDto student2Course1 = new CourseInfoDto("fine art1", 20000, "WEDNESDAY", "LEE", 13L);
+    CourseInfoDto student2Course2 = new CourseInfoDto("math3", 300000, "FRIDAY", "LEE", 10L);
+
+    courseService.registerCourse(student2Course1);
+    courseService.registerCourse(student2Course2);
   }
 }
